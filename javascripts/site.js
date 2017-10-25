95c18dd71c16521b713334ac86ef8bb64cbd17d4
Ilya Potapov <elias.potapov@gmail.com>
2017-10-25 17:15:33 +0300

    Responsive design

--- javascripts/site.js
+++ javascripts/site.js
@@ -1,1 +1,21 @@
 // This is where it all goes :)
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function show_on_click() {
+    document.getElementById("dropdown").classList.toggle("dropdown-show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('dropdown-show')) {
+        openDropdown.classList.remove('dropdown-show');
+      }
+    }
+  }
+} 
