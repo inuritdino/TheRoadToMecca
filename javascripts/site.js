f9f982297d38344399a7844ef11b0beae2e8fa30
Ilya Potapov <elias.potapov@gmail.com>
2018-02-10 19:27:53 +0200

    V. Spirit and flesh

--- javascripts/site.js
+++ javascripts/site.js
@@ -4,6 +4,9 @@ toggle between hiding and showing the dropdown content */
 function show_on_click() {
     document.getElementById("dropdown").classList.toggle("dropdown-show");
 }
+function show_on_click2() {
+    document.getElementById("dropdown-2").classList.toggle("category-show");
+}
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
@@ -18,4 +21,16 @@ window.onclick = function(event) {
       }
     }
   }
+  if (!event.target.matches('.dropbtn2')) {
+
+    var dropdowns = document.getElementsByClassName("category");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('category-show')) {
+        openDropdown.classList.remove('category-show');
+      }
+    }
+  }
 } 
+
